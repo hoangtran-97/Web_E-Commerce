9755976c4f3c37a60910cf37f906e18e6513bea1
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-09-29 16:19:59 +0300

    feat: user schema

---
+++ src/models/User.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document } from "mongoose";
+
+export type UserDocument = Document & {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  isAdmin: boolean;
+  isBanned: boolean;
+  password: string;
+};
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    index: true,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+  },
+  isBanned: {
+    type: Boolean,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model<UserDocument>("User", userSchema);
