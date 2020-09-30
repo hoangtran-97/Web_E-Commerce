f78e891952711b930470b2c5717c31eef25b92bb
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-09-30 11:45:42 +0300

    feat: user router/controller/service

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,3 +3,43 @@ import User, { UserDocument } from "../models/User";
 const create = (user: UserDocument): Promise<UserDocument> => {
     return user.save();
 };
+
+const update = (
+    userId: string,
+    update: Partial<UserDocument>
+): Promise<UserDocument> => {
+    return User.findById(userId)
+        .exec()
+        .then(user => {
+            if (!user) {
+                throw new Error(`User ${userId} not found`);
+            }
+            if (update.userName) {
+                user.userName = update.userName;
+            }
+            if (update.firstName) {
+                user.firstName = update.firstName;
+            }
+            if (update.lastName) {
+                user.lastName = update.lastName;
+            }
+            if (update.email) {
+                user.email = update.email;
+            }
+            if (update.isAdmin) {
+                user.isAdmin = update.isAdmin;
+            }
+            if (update.isBanned) {
+                user.isBanned = update.isBanned;
+            }
+            if (update.password) {
+                user.password = update.password;
+            }
+            return user.save();
+        });
+};
+
+export default {
+    create,
+    update,
+};
