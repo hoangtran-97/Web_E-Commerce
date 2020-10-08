bf8fbc3a75e751eedfad973fbcfad3f7ee3edd9b
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-08 15:46:53 +0300

    feat: addd auth route

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 import { googleTokenId } from "../controllers/auth";
+import { findAll } from "../controllers/movie";
 
 const router = express.Router();
+router.get("/", findAll);
 router.post("/googleTokenId", googleTokenId);
 
 export default router;
