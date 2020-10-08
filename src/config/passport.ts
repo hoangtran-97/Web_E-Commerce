987f825944f52333a7ee2dba413c7f44b8593830
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-08 13:05:43 +0300

    feat: added pasport

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,8 +1,9 @@
-import passport from 'passport'
-import passportLocal from 'passport-local'
-import passportFacebook from 'passport-facebook'
+import passport from "passport";
+import passportLocal from "passport-local";
+import passportFacebook from "passport-facebook";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const GoogleTokenStrategy = require("passport-google-id-token");
+import { Request, Response, NextFunction } from "express";
 
-import { Request, Response, NextFunction } from 'express'
-
-const LocalStrategy = passportLocal.Strategy
-const FacebookStrategy = passportFacebook.Strategy
+const LocalStrategy = passportLocal.Strategy;
+const FacebookStrategy = passportFacebook.Strategy;
