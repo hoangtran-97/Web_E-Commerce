7a441fea2e96180f983e27f8a015d416a696c112
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-12 14:08:19 +0300

    feat: Header Component Desktop

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 import { Search } from "../Search";
 import { ThemeContext } from "../../context";
@@ -10,15 +12,43 @@ export const Header = () => {
     const tx = { color: theme.text };
     return (
         <div className={styles.container} style={fg}>
-            <p style={tx}>E-Commerce</p>
+            <div className={styles.left}>
+                <Player
+                    autoplay
+                    loop
+                    src={require("./shoppin.json")}
+                    speed={100000000}
+                    style={{ height: "70px", width: "70px" }}
+                ></Player>
+                <p style={tx} className={styles.left__text}>
+                    E-Commerce
+                </p>
+            </div>
+
             <Search></Search>
             <div className={styles.right}>
+                <p style={tx}>Welcome guest</p>
                 <p className={styles.login} style={tx}>
                     Login
                 </p>
-                <p className={styles.cart} style={tx}>
-                    Cart
-                </p>
+                <button
+                    className={styles.button__cart}
+                    // style={{ backgroundColor: theme.foreground }}
+                >
+                    <AiOutlineShoppingCart
+                        className={styles.icon__cart}
+                        style={{ color: theme.text }}
+                    ></AiOutlineShoppingCart>
+                    <div
+                        className={styles.cart__counter}
+                        style={{
+                            color: theme.text,
+                            backgroundColor: theme.background,
+                        }}
+                    >
+                        5
+                    </div>
+                </button>
             </div>
         </div>
     );
