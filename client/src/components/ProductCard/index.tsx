e704e93ba99a393bbabc366c753aee58ddd784ad
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-13 14:08:36 +0300

    feat: ProductCard

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -7,14 +7,17 @@ import styles from "./ProductCard.module.css";
 export const ProductCard = ({ product }: ProductCardProps) => {
     const { theme } = useContext(ThemeContext);
     const fg = { backgroundColor: theme.foreground };
+    const tx = { color: theme.text };
     const { name, price, img } = product;
     return (
         <button className={styles.container} style={fg}>
             <div className={styles.img__container}>
                 <img alt="product_image" className={styles.img} src={img}></img>
             </div>
-            <p>{name}</p>
-            <p>{`${price} EUR`}</p>
+            <p className={styles.name} style={tx}>
+                {name}
+            </p>
+            <p className={styles.price} style={tx}>{`${price} EUR`}</p>
         </button>
     );
 };
