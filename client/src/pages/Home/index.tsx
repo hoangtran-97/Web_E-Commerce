9cbd414236032f168fae5fd1c5e418c56cd9fcba
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-15 11:06:54 +0300

    refactor

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -5,14 +5,11 @@ import { useProduct } from "../../hooks/useProduct";
 import { ProductCard } from "../../components/ProductCard";
 import { ThemeContext } from "../../context";
 import styles from "./Home.module.css";
-//Todo Store redux data in memory
 
 export const Home = () => {
     const [products] = useProduct();
     const { theme } = useContext(ThemeContext);
     const bg = { backgroundColor: theme.background };
-
-    console.log(products);
     return (
         <div className={styles.container} style={bg}>
             {products === null ? (
