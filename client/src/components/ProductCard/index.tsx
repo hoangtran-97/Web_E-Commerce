0cc3452e768e33e8e99503318cc1b1775420348d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-15 12:22:30 +0300

    feat: cart indicatior

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -1,14 +1,22 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { AppState } from "../../typings";
 import { ProductCardProps } from "../../typings";
 import { ThemeContext } from "../../context";
 import styles from "./ProductCard.module.css";
 
 export const ProductCard = ({ product }: ProductCardProps) => {
     const { theme } = useContext(ThemeContext);
+    const cart = useSelector((state: AppState) => state.product.inCart);
+    const bg = { backgroundColor: theme.background };
     const fg = { backgroundColor: theme.foreground };
     const tx = { color: theme.text };
     const { name, price, img, _id } = product;
+    const isInCart = cart.find(p => p.name === name);
+    console.log("isINCart", isInCart);
+
     return (
         <Link className={styles.container} style={fg} to={`/product/${_id}`}>
             <div className={styles.img__container}>
@@ -18,6 +26,7 @@ export const ProductCard = ({ product }: ProductCardProps) => {
                 {name}
             </p>
             <p className={styles.price} style={tx}>{`${price} EUR`}</p>
+            {isInCart && <div className={styles.indicator} style={fg}></div>}
         </Link>
     );
 };
