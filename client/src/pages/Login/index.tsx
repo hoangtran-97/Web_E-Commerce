894fa1e9eddf4a6a582168824f22be9987da287e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-15 11:21:30 +0300

    feat: login page

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GoogleLoginButton } from "../../components/GoogleLoginButton";
 
+import { ThemeContext } from "../../context";
 import styles from "./Login.module.css";
 
 export const Login = () => {
-    return <div>Login</div>;
+    const { theme } = useContext(ThemeContext);
+    const fg = { backgroundColor: theme.foreground };
+    const bg = { backgroundColor: theme.background };
+    const tx = { color: theme.text };
+    return (
+        <div className={styles.container} style={bg}>
+            <GoogleLoginButton></GoogleLoginButton>
+            <p>Normal registration comming soon</p>
+        </div>
+    );
 };
