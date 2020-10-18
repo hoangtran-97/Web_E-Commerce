c84bc39ac1bf0133eba159b03e7fea2ecc135e10
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-18 12:17:18 +0300

    feat: redux remove product

--- client/src/components/CartItem/index.tsx
+++ client/src/components/CartItem/index.tsx
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
-import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { removeProduct } from "../../redux/actions";
-import { CartItemProps } from "../../typings";
+import { CartItemProps, AppState, Product } from "../../typings";
 import { ProductCard } from "../ProductCard";
 import { ThemeContext } from "../../context";
 import styles from "./CartItem.module.css";
 
+const handleRemove = (dispatch: Dispatch, item: Product) => {
+    dispatch(removeProduct(item));
+};
+
 export const CartItem = ({ item }: CartItemProps) => {
     const dispatch = useDispatch();
     const { theme } = useContext(ThemeContext);
+    const { currentUser, token } = useSelector((state: AppState) => state.user);
     const fg = { backgroundColor: theme.foreground };
     const tx = { color: theme.text };
     return (
@@ -18,7 +24,7 @@ export const CartItem = ({ item }: CartItemProps) => {
             <button
                 className={styles.button}
                 style={{ ...fg, ...tx }}
-                onClick={() => dispatch(removeProduct(item))}
+                onClick={() => handleRemove(dispatch, item)}
             >
                 Remove from cart
             </button>
