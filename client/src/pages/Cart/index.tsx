c52a898332a69a3de809fa48e5fbb6da48f51ff4
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-18 10:33:10 +0300

    feat: cart page

--- client/src/pages/Cart/index.tsx
+++ client/src/pages/Cart/index.tsx
@@ -1,11 +1,15 @@
 import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
+import { AppState } from "../../typings";
 import { ThemeContext } from "../../context";
 import styles from "./Cart.module.css";
 
 export const Cart = () => {
     const { theme } = useContext(ThemeContext);
+    const cart = useSelector((state: AppState) => state.product.inCart);
     const bg = { backgroundColor: theme.background };
+
     return (
         <div className={styles.container} style={bg}>
             Cart
