f23b3df27906b3ccd0963177facfa93c59275706
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-18 12:58:03 +0300

    feat: profile component

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
+import { AppState } from "../../typings";
+import { ThemeContext } from "../../context";
 import styles from "./Profile.module.css";
 
 export const Profile = () => {
-    return <div>Profile</div>;
+    const { theme } = useContext(ThemeContext);
+    const bg = { backgroundColor: theme.background };
+    const user = useSelector((state: AppState) => state.user.currentUser);
+    const { userName, isAdmin } = user;
+    return (
+        <div className={styles.container} style={bg}>
+            <p>
+                {userName} {isAdmin && <span>- ADMIN</span>}
+            </p>
+        </div>
+    );
 };
