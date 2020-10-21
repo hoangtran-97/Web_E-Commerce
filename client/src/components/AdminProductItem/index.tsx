c14fc6110656fcaef194a2a7080161e8e3599e5e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-21 12:02:11 +0300

    feat: admin component

--- client/src/components/AdminProductItem/index.tsx
+++ client/src/components/AdminProductItem/index.tsx
@@ -6,12 +6,15 @@ import { ThemeContext } from "../../context";
 import styles from "./AdminProductItem.module.css";
 
 export const AdminProductItem = ({ product }: AdminProductItemProps) => {
-    const { _id, name } = product;
+    const { _id, name, img } = product;
     const { theme } = useContext(ThemeContext);
     const tx = { color: theme.text };
     const fg = { backgroundColor: theme.foreground };
     return (
-        <div className={styles.container} style={{ ...fg, ...tx }}>
+        <div
+            className={styles.container}
+            style={{ ...tx, backgroundImage: `url(${img})` }}
+        >
             <div>
                 <h3>{name}</h3>
                 <h5>{_id}</h5>
