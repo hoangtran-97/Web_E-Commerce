8ec7f4f5c55d4340d39b45dcac31aa6e9a2b2b3b
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-23 15:06:39 +0300

    feat: ban/unban user

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
-import { Product, HomeProps } from "../../typings";
+import { Product, HomeProps, AppState } from "../../typings";
 import { useProduct } from "../../hooks/useProduct";
 import { ProductCard } from "../../components/ProductCard";
 import { ThemeContext } from "../../context";
@@ -9,7 +10,12 @@ import styles from "./Home.module.css";
 export const Home = ({ query }: HomeProps) => {
     const [products] = useProduct(query);
     const { theme } = useContext(ThemeContext);
+    const { currentUser } = useSelector((state: AppState) => state.user);
+    const { isBanned } = currentUser;
     const bg = { backgroundColor: theme.background };
+    if (isBanned) {
+        return <div> YOU ARE BANNED</div>;
+    }
     return (
         <div className={styles.container} style={bg}>
             {products.length === 0 ? (
