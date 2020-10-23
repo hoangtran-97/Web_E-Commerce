71b6808e2ee2701f58638a4dd3dd670e2e89289b
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-10-23 13:38:09 +0300

    feat: logout

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { useHistory } from "react-router-dom";
 import "react-tabs/style/react-tabs.css";
 
 import { AppState } from "../../typings";
@@ -10,20 +9,14 @@ import { AdminProductItem } from "../../components/AdminProductItem";
 import { AdminAddProduct } from "../../components/AdminAddProduct";
 import { useUser } from "../../hooks/useUser";
 import { AdminUserItem } from "../../components/AdminUserItem";
-import { removeUser } from "../../redux/actions";
-import styles from "./Profile.module.css";
 import { GoogleLogoutButton } from "../../components/GoogleLogoutButton";
+import styles from "./Profile.module.css";
 
 export const Profile = () => {
     const { theme } = useContext(ThemeContext);
     const { currentUser, token } = useSelector((state: AppState) => state.user);
     const { list } = useSelector((state: AppState) => state.product);
-    const dispatch = useDispatch();
     const [users] = useUser();
-    const history = useHistory();
-    const goHome = () => {
-        history.push("/");
-    };
     const bg = { backgroundColor: theme.background };
     const tx = { color: theme.text };
     const { userName, isAdmin } = currentUser;
@@ -32,14 +25,6 @@ export const Profile = () => {
             <p>
                 {userName} {isAdmin && <span>- ADMIN</span>}
             </p>
-            <button
-                onClick={() => {
-                    dispatch(removeUser(currentUser));
-                    goHome();
-                }}
-            >
-                Logout
-            </button>
             <GoogleLogoutButton></GoogleLogoutButton>
             {isAdmin && (
                 <Tabs className={styles.tab}>
