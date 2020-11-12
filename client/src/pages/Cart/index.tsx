a24fb62f7e9fde450e5c2b3abc99188a3904fc8d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-11-12 11:06:21 +0200

    feat: cart total price

--- client/src/pages/Cart/index.tsx
+++ client/src/pages/Cart/index.tsx
@@ -10,18 +10,22 @@ export const Cart = () => {
     const { theme } = useContext(ThemeContext);
     const cart = useSelector((state: AppState) => state.product.inCart);
     const bg = { backgroundColor: theme.background };
-
+    const sum = cart.reduce((a, b) => a + b.price * b.quantity, 0);
+    console.log(sum);
     return (
         <div className={styles.container} style={bg}>
             <h2 className={styles.title}>Your shopping cart</h2>
             {cart.length === 0 ? (
                 <h3 className={styles.title}>is empty!</h3>
             ) : (
-                <div className={styles.container__cart}>
-                    {cart.map(item => (
-                        <CartItem key={item._id} item={item}></CartItem>
-                    ))}
-                </div>
+                <>
+                    <h3 className={styles.title}>Total cost: {sum} €</h3>
+                    <div className={styles.container__cart}>
+                        {cart.map(item => (
+                            <CartItem key={item._id} item={item}></CartItem>
+                        ))}
+                    </div>
+                </>
             )}
         </div>
     );
