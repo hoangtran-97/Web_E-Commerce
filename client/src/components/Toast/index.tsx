ee3bb7347c3887a00958c547a39536c3efe31d5f
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-11-12 11:59:42 +0200

    fix: toast switch cases

--- client/src/components/Toast/index.tsx
+++ client/src/components/Toast/index.tsx
@@ -7,17 +7,18 @@ export const Toast = () => {
     const toastState = useSelector((state: AppState) => state.ui.toast);
     const { message, intent } = toastState;
     useEffect(() => {
-        console.log(toastState);
         if (message) {
             if (message.length > 0) {
-                console.log("toasted");
                 switch (intent) {
                     case IntentType.ERROR:
                         toast.error(message);
+                        break;
                     case IntentType.SUCCESS:
                         toast.success(message);
+                        break;
                     case IntentType.INFO:
                         toast.info(message);
+                        break;
                     default:
                         toast(message);
                 }
